af489b7ad0590e4f2db531bffa17c0622ce40a3d
Brandon <obdnanr@gmail.com>
2015-06-07 18:03:01 -0400

    ATM

---
+++ atmbeforegiantchange.py
@@ -0,0 +1,88 @@
+import os
+# Create accounts
+class Account(object):
+	def __init__(self, initial):
+		self.balance = initial
+	
+	def deposit(self, amt):
+		self.balance = self.balance + amt
+	
+#Withdraw money if the balance is greater than 0
+	def withdraw(self,amt):
+		if (self.balance - amt) > 0.0:
+			self.balance = self.balance - amt
+			print '\n\n\n\n\nYour balance is now $', self.balance
+			menu()
+		else:
+			print 'Insufficient funds'
+			login()
+
+#Get the account balance and print it to the screen
+	def getbalance(self):
+		print '\n\n\n\n\nYour balance is $', self.balance
+		menu()		
+		return self.balance
+	
+	def password(self):
+		if password == password:
+			print 'it worked'
+			return True
+#menu function	
+def menu():
+	menu = """\n\n\n\n\n1) Fast Cash $40\t\t3) Withdraw\n\n2) Account Balance\t\t4) Deposit\n\n5) Exit"""
+	sel = int(raw_input(menu))
+#Fast cash calls the class Account and uses the withdraw function
+#to withdraw $40 quickly.
+	if sel == 1:
+		os.system('clear')
+		jon.withdraw(40.0)
+		
+#This will display the user's account balance
+	elif sel == 2:
+		os.system('clear')
+		jon.getbalance()
+
+#This will ask the user for how much they want to withdraw and
+#convert it to a float
+	elif sel == 3:
+		os.system('clear')	
+		amt = float(raw_input("""\n\n\n\n\n
+Please enter your the amount you wish to withdraw in $20 increments.\n"""))
+		os.system('clear')
+		ans = int(raw_input("""Are you sure you want to withdraw $ %d?\n1) Yes\n2) No\n""" % (amt)))
+		if ans == 1:
+			os.system('clear')
+			jon.withdraw(amt)
+		else:
+			pass
+
+#This is for depositing
+	elif sel == 4:
+		pass
+
+	elif sel == 5:
+		os.system('clear')		
+		login()#call the login function you about to write
+	else:
+		print('Wrong selection please login')
+		login()
+
+#end of menu function	
+
+jon=Account(110.00)
+jon.pin = int(1234)
+
+#username = raw_input('Enter your user name\n')
+#login function checks the pin and proceeds if it's correct
+def login():
+	pin = int(raw_input('Enter your pin\n'))
+	if pin == jon.pin:
+		menu()
+	else:
+		os.system('clear')
+		print('Wrong pin, the police are on the way.')
+		login()
+
+login()
+
+
